b89d4f465c7e0d93af14e9ba3b63752f21ba92aa
Caien1 <pulwama01@gmail.com>
2025-02-03 18:53:27 +0530

    Can create a window now

--- code/main.cpp
+++ code/main.cpp
@@ -1,14 +1,90 @@
 
-#include<windows.h>
+#include <windows.h>
+#include<stdio.h>
 
-
-int WINAPI WinMain(
-    HINSTANCE hInstance, 
-    HINSTANCE hPrevInstance, 
-    PSTR lpCmdLine,
-    int nCmdShow)
+LRESULT CALLBACK Wndproc(
+    HWND Window,
+    UINT Message,
+    WPARAM WParam,
+    LPARAM LParam)
 {
-    MessageBox(0,"Hello there ","Whats up", MB_OK|MB_ICONINFORMATION);
-    return (0);
+    LRESULT Result = 0;
+
+    switch (Message)
+    {
+    case WM_SIZE:
+    {
+    }
+    case WM_PAINT:{
+
+    }
+    break;
+    default:
+    {
+        Result = DefWindowProc(Window, Message, WParam, LParam);
+    }
+    break;
+    }
+
+    return (Result);
 }
 
+int WINAPI WinMain(
+    HINSTANCE Instance,
+    HINSTANCE PrevInstance,
+    PSTR CmdLine,
+    int CmdShow)
+{
+    WNDCLASS WindowClass ={0};
+    WindowClass.style = CS_OWNDC | CS_HREDRAW | CS_VREDRAW;
+    WindowClass.lpfnWndProc = Wndproc;
+    WindowClass.hInstance = Instance;
+    WindowClass.lpszClassName = "HereWeGoAgain";
+
+    // Registering the window
+    if (RegisterClass(&WindowClass))
+    {
+        HWND WindowHandler = CreateWindowExA(
+            0,
+            WindowClass.lpszClassName,
+            "Fist WIndow",
+            WS_OVERLAPPEDWINDOW|WS_VISIBLE,
+            CW_USEDEFAULT,
+            CW_USEDEFAULT,
+            CW_USEDEFAULT,
+            CW_USEDEFAULT,
+            0,
+            0,
+            Instance,
+            0);
+
+            if(WindowHandler){
+                MSG Message;
+
+                for(;;){
+                    BOOL MessageResult =GetMessageA(&Message,0,0,0);
+                    if(MessageResult> 0){
+                            TranslateMessage(&Message);
+                            DispatchMessage(&Message);
+                    }
+                    else{
+                        printf("Couldnt generate a Message result");
+
+                        break;
+                    }
+
+                }
+
+
+            }else{
+                //Todo(Caien):Logging
+
+                printf("Couldnt generate a window handler");
+            }
+    }
+    else
+    {
+    }
+
+    return (0);
+}
